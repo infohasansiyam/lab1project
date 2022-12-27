d37f93c3264587644e62a6381fb8ab9d3d4dc467
infohasansiyam <infohasansiyam@gmail.com>
2022-12-27 11:32:32 +0600

    lab

--- lab1project/courseApp/views.py
+++ lab1project/courseApp/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def first(request):
-    return HttpResponse("<h1>this is a first response</h1>")
+    return render(request,'index.html')
 
 def second(requst):
-    return HttpResponse("this is a second response")
+    return render("this is a second response")
 
 def third(requst):   
-    return HttpResponse("this is a third response")
+    return render("this is a third response")
 
 
